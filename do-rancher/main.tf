f41fed03e66593dfde7a0bf85607f6d8af2e2d41
Max Sokolovsky <genexpr@protonmail.com>
2023-07-17 23:48:43 -0400

    Add a single-node Rancher deployment

---
+++ do-rancher/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2"
+    }
+    time = {
+      source = "hashicorp/time"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_ssh_key" "docker_pub_key" {
+  name       = "max-docker-key"
+  public_key = file(var.ssh_public_key_file)
+}
+
+resource "digitalocean_droplet" "dockernode" {
+  image    = "132425012" # Docker 23.0.6 on Ubuntu 22.04.
+  name     = format("%s", var.do_node_name_prefix)
+  region   = var.do_region
+  size     = var.do_droplet_size
+  ssh_keys = [digitalocean_ssh_key.docker_pub_key.id]
+  provisioner "remote-exec" {
+    inline = [
+      <<EOT
+      docker run -d -p 80:80 -p 443:443 \
+        -e CATTLE_AGENT_IMAGE=rancher/rancher-agent:${var.rancher_agent_version} \
+        -e CATTLE_BOOTSTRAP_PASSWORD=${var.bootstrap_password} \
+        --privileged --name rancher rancher/rancher:${var.rancher_version} >/dev/null 2>&1
+      EOT
+    ]
+    connection {
+      host  = self.ipv4_address
+      type  = "ssh"
+      user  = var.do_node_user
+      agent = true
+    }
+  }
+}
+
+resource "digitalocean_record" "dns" {
+  name   = var.subdomain
+  domain = var.domain
+  type   = "A"
+  value  = digitalocean_droplet.dockernode.ipv4_address
+}
+
+output "rancher_server_url" {
+  depends_on = [
+    digitalocean_record.dns,
+  ]
+  description = "URL of the running Rancher server"
+  value       = "https://${digitalocean_record.dns.name}.${digitalocean_record.dns.domain}"
+}
+
+output "droplet_ip" {
+  description = "Droplet IP address"
+  value       = digitalocean_droplet.dockernode.ipv4_address
+}
---
+++ do-rancher/variables.tf
@@ -0,0 +1,58 @@
+variable "do_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "install_rancher" {
+  type    = bool
+  default = true
+}
+
+variable "bootstrap_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "do_node_name_prefix" {
+  type    = string
+  default = "max-node"
+}
+
+variable "rancher_version" {
+  type    = string
+  default = "v2.7.5"
+}
+
+variable "rancher_agent_version" {
+  type    = string
+  default = "v2.7.5"
+}
+
+variable "do_node_user" {
+  type    = string
+  default = "root"
+}
+
+variable "do_region" {
+  type    = string
+  default = "nyc1"
+}
+
+variable "do_droplet_size" {
+  type    = string
+  default = "s-4vcpu-8gb-intel"
+}
+
+variable "ssh_public_key_file" {
+  type        = string
+  default     = "./id_rsa.pub"
+  description = "which SSH public key to use for access to nodes"
+}
+
+variable "domain" {
+  type = string
+}
+
+variable "subdomain" {
+  type = string
+}
