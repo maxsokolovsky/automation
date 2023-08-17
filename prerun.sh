be61f8b3097408386442d34f71e83da8ce35875d
Max Sokolovsky <genexpr@protonmail.com>
2023-08-17 10:19:57 -0400

    Ask for DO token only if it's not already set

--- prerun.sh
+++ prerun.sh
@@ -3,8 +3,10 @@
 
 [ -f "id_rsa" ] || ssh-keygen
 
-echo -n 'DO Token: '
-read -sr TF_VAR_do_token
-export TF_VAR_do_token
-echo
+if [ -z "$TF_VAR_do_token" ]; then
+    echo -n 'DO Token: '
+    read -sr TF_VAR_do_token
+    export TF_VAR_do_token
+    echo
+fi
 ssh-add id_rsa
