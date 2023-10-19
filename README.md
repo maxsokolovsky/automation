cd9fe9bfae5628158553ee38407a379dcc8f49d8
Max Sokolovsky <genexpr@protonmail.com>
2023-10-18 21:27:28 -0400

    Add a Droplet owner tag

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ### Terraform
 
+- do-droplet - one or more bare DigitalOcean droplets
 - do-rke - a two-node (one worker, one control plane) Kubernetes cluster on RKE
 - do-rancher - a single-node Rancher deployment in Docker
 - do-rancher-ha - an HA deployment of Rancher on RKE
--- do-droplet/main.tf
+++ do-droplet/main.tf
@@ -19,16 +19,21 @@ resource "digitalocean_ssh_key" "docker_pub_key" {
   public_key = file(var.ssh_public_key_file)
 }
 
-resource "digitalocean_droplet" "dockernodes" {
+resource "digitalocean_droplet" "nodes" {
   image    = "ubuntu-20-04-x64"
   count    = var.droplet_count
   name     = format("%s-%d", var.do_node_name_prefix, count.index + 1)
   region   = var.do_region
   size     = var.do_droplet_size
   ssh_keys = [digitalocean_ssh_key.docker_pub_key.id]
+  tags     = [digitalocean_tag.owner.id]
+}
+
+resource "digitalocean_tag" "owner" {
+  name = "owner:max"
 }
 
 output "droplet_ip" {
   description = "Droplet IP address"
-  value       = digitalocean_droplet.dockernodes[*].ipv4_address
+  value       = digitalocean_droplet.nodes[*].ipv4_address
 }
